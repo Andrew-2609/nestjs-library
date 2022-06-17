4fe3d8b45561979f045347e708d58130a3c100f3
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-06-16 22:05:58 -0300

    feat: adapt getProducts() to return in memory Products array

--- src/products.controller.ts
+++ src/products.controller.ts
@@ -7,12 +7,19 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import { Product } from './product.model';
 
 @Controller('products')
 export class ProductsController {
+  products: Product[] = [
+    new Product('BK01', 'The Call of Cthulhu', 9.9),
+    new Product('BK02', 'The Dunwich Horror', 11.5),
+    new Product('BK03', 'The Hounds of Baskervilles', 10.5),
+  ];
+
   @Get()
-  getProducts(): string {
-    return 'This action returns all products';
+  getProducts(): Product[] {
+    return this.products;
   }
 
   @Get(':id')
