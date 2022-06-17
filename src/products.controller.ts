9181d99560e5c916275cdbeda80116d7153b02b2
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-06-16 21:47:08 -0300

    feat: define an endpoint to get a single product

--- src/products.controller.ts
+++ src/products.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
   @Get()
   getProducts(): string {
     return 'This action returns all products';
+  }
+
+  @Get(':id')
+  getProduct(@Param() params): string {
+    return `This action returns the project of id: ${params.id}`;
   }
 }
