8b32c1ff398b2b27cd83c6283c4fce686cc58f62
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-06-17 22:23:44 -0300

    feat: import SequelizeModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Book } from './book.model';
 import { BooksController } from './books.controller';
 import { BooksService } from './books.service';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: '172.17.0.2',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'nest_library',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+
+    SequelizeModule.forFeature([Book]),
+  ],
   controllers: [AppController, BooksController],
   providers: [AppService, BooksService],
 })
